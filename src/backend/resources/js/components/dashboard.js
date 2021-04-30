ae9a60c4019b073c871a39189e3b86c17c798ea3
SPROBENET\chiong.r <chiong.r@sprobe.com>
2021-04-30 10:48:52 +0800

    disable resizing on small widgets
    Reviewers: santos.ma
    
    Reviewed By: santos.ma
    
    Maniphest Tasks: T34518
    
    Differential Revision: https://phabricator.sprobe.ph/D25334

--- src/backend/resources/js/components/dashboard.js
+++ src/backend/resources/js/components/dashboard.js
@@ -23,15 +23,15 @@ const Dashboard = () => {
   }
 
   const widgets =[
-    {component: <Welcome/>, label: 'Welcome', static: true,style:'staticWidgets', className: '', state: true, x: 0, y: 0, w: 4, h: .25},
-    {component:<CompanyDashboardChart/>, label: 'Dashboard Charts', static: false, className:'', state: true, x: 0, y: 0, w: 4, h: 2, minW: 2},
-    {component: <Services/>, label: 'Contracted Services', static: false, className:'', state: true, x: 0, y: 0, w: 2, h: 1.25, minW: 2},
-    {component: <Linkage/>, label: 'Linkage Services', static: false,className:'', state: true, x: 2, y:3 , w: 2, h: 1.25, minW: 2},
-    {component: <ServiceUsage/>,label: 'Status of Service Usage',static: false, className: '', state: true, x: 0, y: 4, w: 4, h: 1.25, minW: 2},
-    {component: <BillingHistory/>, label: 'Billing History', static: false, className: '', state: true, x: 4, y: 0, w: 3, h: 2.50, minW: 2},
-    {component: <Notification/>,label: 'Notifications', static: false, className: '', state: true,x: 4, y:2, w: 3, h: 2.25, minW: 1},
-    {component: <Settings showWidgets={showWidgetSettingsHandler}/>, label: 'Settings', static: false, className: '', state: true, x: 7, y: 0, w: 3, h: 2.50},
-    {component: <Purchase/>,label: 'Purchase History', static: false, className: '', state: true, x: 7, y: 4, w: 3, h: 2.25, minW: 2}
+    {component: <Welcome/>, label: 'Welcome', static: true,style:'staticWidgets', className: '', state: true, x: 0, y: 0, w: 4, h: .25,isResizable:false},
+    {component:<CompanyDashboardChart/>, label: 'Dashboard Charts', static: false, className:'', state: true, x: 0, y: 0, w: 4, h: 2, minW: 2,isResizable:true},
+    {component: <Services/>, label: 'Contracted Services', static: false, className:'', state: true, x: 0, y: 0, w: 2, h: 1.25, minW: 2,isResizable:false},
+    {component: <Linkage/>, label: 'Linkage Services', static: false,className:'', state: true, x: 2, y:3 , w: 2, h: 1.25, minW: 2, isResizable:false},
+    {component: <ServiceUsage/>,label: 'Status of Service Usage',static: false, className: '', state: true, x: 0, y: 4, w: 4, h: 1.25, minW: 2, isResizable:true},
+    {component: <BillingHistory/>, label: 'Billing History', static: false, className: '', state: true, x: 4, y: 0, w: 3, h: 2.50, minW: 2, isResizable:true},
+    {component: <Notification/>,label: 'Notifications', static: false, className: '', state: true,x: 4, y:2, w: 3, h: 2.25, minW: 1, isResizable:true},
+    {component: <Settings showWidgets={showWidgetSettingsHandler}/>, label: 'Settings', static: false, className: '', state: true, x: 7, y: 0, w: 3, h: 2.50, isResizable:true},
+    {component: <Purchase/>,label: 'Purchase History', static: false, className: '', state: true, x: 7, y: 4, w: 3, h: 2.25, minW: 2, sResizable:true}
   ]
   const [widgetState,setWidgetState]=useState(widgets)
   
@@ -54,7 +54,7 @@ const Dashboard = () => {
           isBounded={true}>
           {
             widgetState.filter(widget => widget.state !== false).map((item, index) => {
-              return (<div key={index} data-grid={{x: item.x, y: item.y, w: item.w, h: item.h,static:item.static}} >{item.component}</div>)
+              return (<div key={index} data-grid={{x: item.x, y: item.y, w: item.w, h: item.h,static:item.static, isResizable:item.isResizable}} >{item.component}</div>)
             })
           }
         </ResponsiveGridLayout>
