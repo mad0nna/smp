0793bd88b10e8f40e45b23481e166c7201a6f915
SPROBENET\chiong.r <chiong.r@sprobe.com>
2021-05-05 13:13:46 +0800

    billing history data adjustment per client feedback
    Summary: data adjustment per client feedback
    
    Reviewers: santos.ma
    
    Reviewed By: santos.ma
    
    Maniphest Tasks: T34624
    
    Differential Revision: https://phabricator.sprobe.ph/D25392

--- src/backend/resources/js/components/companyBilling.js
+++ src/backend/resources/js/components/companyBilling.js
@@ -11,11 +11,16 @@ class CompanyBilling extends React.Component{
     super(props)
     this.state = {
       billingList: [
-        {no: 'KOT - INV023', invoiceName: 'KOT - TM 定期購読', requestDate: '2021年4月5日', dueDate: '2021年4月30日', requestBy: '株式会社町田', state: '未払い'},
-        {no: 'KOT - INV022', invoiceName: 'KOT - SL 定期購読', requestDate: '2021年4月5日', dueDate: '2021年4月30日', requestBy: '株式会社町田', state: '未払い'},
-        {no: 'KOT - INV021', invoiceName: 'KOT - DA 定期購読', requestDate: '2021年4月5日', dueDate: '2021年4月30日', requestBy: '株式会社町田', state: '未払い'},
-        {no: 'KOT - INV020', invoiceName: 'KOT - TM 定期購読', requestDate: '2021年3月5日', dueDate: '2021年3月30日', requestBy: '株式会社町田', state: '支払い済み'},
-        {no: 'KOT - INV019', invoiceName: 'KOT - SL 定期購読', requestDate: '2021年3月5日', dueDate: '2021年3月30日', requestBy: '株式会社町田', state: '支払い済み'}
+        {no: 'INV00024143', billingName: 'KOT-TM定期購読', billingDate: '2021年5月1日', dueDate: '2021年5月31日', amount: '¥10,890', paidOn: '未払い', status: '未払い'},
+        {no: 'INV00024011', billingName: 'KOT-SL 定期購読', billingDate: '2021年4月1日', dueDate: '2021年4月30日', amount: '¥11,220', paidOn: '2021年4月30日', status: '支払い済み'},
+        {no: 'INV00023561', billingName: 'KOT-TM 定期購読', billingDate: '2021年3月1日', dueDate: '22021年3月31日', amount: '¥10,890', paidOn: '2021年4月30日', status: '支払い済み'},
+        {no: 'INV00012456', billingName: 'KOT-XC 定期購読', billingDate: '2021年3月1日', dueDate: '2021年3月31日', amount: '¥1,542,000', paidOn: '未払い', status: '未払い'},
+        {no: 'INV00073895', billingName: 'KOT-SM 定期購読', billingDate: '2021年1月1日', dueDate: '2021年1月31日', amount: '¥5,040,220', paidOn: '未払い', status: '未払い'},
+        {no: 'INV00072546', billingName: 'KOT-RT 定期購読', billingDate: '2021年3月1日', dueDate: '2021年3月31日', amount: '¥78,505,500', paidOn: '未払い', status: '未払い'},
+        {no: 'INV00023451', billingName: 'KOT-WH 定期購読', billingDate: '2021年4月1日', dueDate: '2021年4月30日', amount: '¥2,024,520', paidOn: '2021年4月25日', status: '支払い済み'},
+        {no: 'INV00078511', billingName: 'KOT-NB 定期購読', billingDate: '2021年3月1日', dueDate: '2021年3月31日', amount: '¥3,359,950', paidOn: '2021年3月7日', status: '支払い済み'},
+        {no: 'INV00065224', billingName: 'KOT-RT 定期購読', billingDate: '2021年3月1日', dueDate: '2021年3月31日', amount: '¥11,520', paidOn: '2021年3月7日', status: '支払い済み'},
+        {no: 'INV00012575', billingName: 'KOT-SL 定期購読', billingDate: '2021年1月1日', dueDate: '2021年1月31日', amount: '¥45,234,770', paidOn: '2021年1月1日', status: '支払い済み'},
       ]
     }
   }
@@ -24,11 +29,11 @@ class CompanyBilling extends React.Component{
     return (
       <div className="relative px-10 py-5 bg-mainbg">
         <div className="w-full h-full overflow-hidden relative  rounded-lg border-2 border-gray-200 ">
-          <div id="widget-header" className="max-w-full h-32 bg-white box-border align-middle p-4 relative">
+          <div id="widget-header" className="max-w-full h-24 bg-white box-border align-middle p-4 relative">
             <img src={BillingIcon} className="w-auto h-7 float-left ml-4"/>
             <div id="widget-name" className="text-primary-200 text-xl font-sans font-bold ml-4 float-left">請求履歴</div>
             <div id="widget-name" className="float-right mr-12">
-              <div className="table-cell relative h-24 w-full align-middle">
+              <div className="table-cell relative h-20 w-full align-middle">
                 <div id="search-bar" className="bg-mainbg h-12 rounded-3xl w-96 mx-0 my-auto">
                   <svg className="text-gray-500 fill-current w-auto h-11 float-left mt-0.5 p-3" xmlns="http://www.w3.org/2000/svg" x="30px" y="30px"
                     viewBox="0 0 487.95 487.95"
@@ -49,12 +54,14 @@ class CompanyBilling extends React.Component{
           </div>
           <div id="widget-body" className="h-50 w-full bg-white overflow-hidden">
             <table className="w-full h-auto text-center">
-              <thead className="bg-table-header-Gray-100 text-table-header-Gray-400 text-gray-400 h-3 font-bold text-lg tracking-tight">
+              <thead className="bg-table-header-Gray-100 text-gray-400 h-3 font-bold text-lg tracking-tight">
                 <tr className="h-12 w-12">
-                  <td>請求書 No.</td>
+                  <td>請求書番号</td>
                   <td>請求書名</td>
                   <td>請求日</td>
-                  <td>支払い期限</td>
+                  <td>支払期限</td>
+                  <td>請求額</td>  
+                  <td>支払日</td>
                   <td>状態</td>
                   <td>操作</td>
                 </tr>
@@ -62,15 +69,17 @@ class CompanyBilling extends React.Component{
               <tbody className="transform even:bg-gray-500">
                 {
                   this.state.billingList.map((item, index) => {
-                    let statusHighlight = (item.state === '未払い') ? 'text-red-600' : ''
+                    let statusHighlight = (item.status === '未払い') ? 'text-red-600' : ''
                     return (
-                      <tr className="stripe-table-row h-32 font-sans font-bold text-md text-gray-600" key={index}>
-                        <td className="w-12">{item.no}</td>
-                        <td className="w-12">{item.invoiceName}</td>
-                        <td className="w-12">{item.requestDate}</td>
-                        <td className="w-12">{item.dueDate}</td>
-                        <td className={statusHighlight+ ' w-12'}>{item.state}</td>
-                        <td className="w-12 cursor-pointer"><img src={PdfIcon} className="mx-auto w-6 h-auto"/> </td>
+                      <tr className="stripe-table-row h-16 font-sans font-bold text-md text-gray-600" key={index}>
+                        <td className="w-10">{item.no}</td>
+                        <td className="w-10">{item.billingName}</td>
+                        <td className="w-10">{item.billingDate}</td>
+                        <td className="w-10">{item.dueDate}</td>
+                        <td className="w-10">{item.amount}</td>
+                        <td className="w-10">{item.paidOn}</td>
+                        <td className={statusHighlight+ ' w-10'}>{item.status}</td>
+                        <td className="w-10 cursor-pointer"><img src={PdfIcon} className="mx-auto w-6 h-auto"/> </td>
                       </tr>
                     )
                   })
