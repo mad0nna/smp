d6fd728e9d21ce459e3d8932d49b8ffe28c5904c
Mark Anthony Santos <markanthonysantos2497@gmail.com>
2021-05-06 12:32:39 +0800

    disable link
    Reviewers: chiong.r
    
    Reviewed By: chiong.r
    
    Maniphest Tasks: T34607
    
    Differential Revision: https://phabricator.sprobe.ph/D25400

--- src/backend/resources/js/components/Navigation.js
+++ src/backend/resources/js/components/Navigation.js
@@ -57,9 +57,9 @@ class Navigation extends React.Component {
     this.adminNavigation = {
       logo: KotLogo,
       navItem: [
-        {label: 'ダッシュボード', url: '/admin/dashboard', iconNormal: 'bg-dashboard-icon', iconHover: 'group-hover:bg-dashboard-icon-hover', iconActive: 'bg-dashboard-icon-hover', iconSize: 'h-8 w-9', isActive: false, extraStyle: ''},
+        {label: 'ダッシュボード', url: '#', iconNormal: 'bg-dashboard-icon', iconHover: 'group-hover:bg-dashboard-icon-hover', iconActive: 'bg-dashboard-icon-hover', iconSize: 'h-8 w-9', isActive: false, extraStyle: ''},
         {label: 'アカウント', url: '/admin/accounts', iconNormal: 'bg-account-list-icon', iconHover: 'group-hover:bg-account-list-icon-hover', iconActive: 'bg-account-list-icon-hover', iconSize: 'h-8 w-9', isActive: false, extraStyle: ''},
-        {label: 'ドキュメント', url: '/admin/documents', iconNormal: 'bg-document-icon', iconHover: 'group-hover:bg-document-icon-hover', iconActive: 'bg-document-icon-hover', iconSize: 'h-8 w-9', isActive: false, extraStyle: ''},
+        {label: 'ドキュメント', url: '#', iconNormal: 'bg-document-icon', iconHover: 'group-hover:bg-document-icon-hover', iconActive: 'bg-document-icon-hover', iconSize: 'h-8 w-9', isActive: false, extraStyle: ''},
       ],
       dropDownNav: {
         title: '管理者',
