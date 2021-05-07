45fa1061aa5da88b3651ee79e21a85e217e925d8
roda <chiong.r@sprobe.com>
2021-05-07 20:39:46 +0800

    purchase history data adjustment
    Reviewers: alcuino.im
    
    Reviewed By: alcuino.im
    
    Maniphest Tasks: T34681
    
    Differential Revision: https://phabricator.sprobe.ph/D25418

--- src/backend/resources/js/components/purchaseHistory.js
+++ src/backend/resources/js/components/purchaseHistory.js
@@ -5,9 +5,9 @@ class PurchaseHistory extends React.Component {
   constructor(props) {
     super(props)
     this.purchaseHistory = [
-      {category: '指紋スキャナー', date:'2020年12月10日 - ¥ 10 , 000', billTo: '株式会社町田', paymentDate: '2020年12月11日'},
-      {category: '指紋スキャナー', date:'2020年12月10日 - ¥ 10 , 000', billTo: '株式会社町田', paymentDate: '2020年12月11日'},
-      {category: '指紋スキャナー', date:'2020年12月10日 - ¥ 10 , 000', billTo: '株式会社町田', paymentDate: '2020年12月11日'}
+      {productName:'指紋スキャナー', purchaseDate: '2021年3月5日', numberOfUnits: '5', amount:'¥25,000',  status:'支払済み' },
+      {productName:'指紋スキャナー', purchaseDate: '2021年2月6日', numberOfUnits: '7', amount:'¥35,000',  status:'支払済み' },
+      {productName:'指紋スキャナー', purchaseDate: '2020年12月4日', numberOfUnits: '10', amount:'¥50,000',  status:'支払済み' },
     ]
   }
 
@@ -18,25 +18,31 @@ class PurchaseHistory extends React.Component {
     }
     return(
       <div className="w-full h-full relative group">
-        <div className={'absolute w-12 h-5 -top-4 px-1 pt-0.5 right-6 text-center font-sans text-gray-500 bg-white font-sans text-xxs leading-2 rounded-md border-gray-200 border-2 cursor-move hidden ' + showMoveButton}>Move</div>
+        <div className={'absolute w-12 h-5 -top-4 px-1 pt-0.5 right-6 text-center font-sans text-gray-500 bg-white text-xxs leading-2 rounded-md border-gray-200 border-2 cursor-move hidden ' + showMoveButton}>Move</div>
         <div className="w-full h-full overflow-hidden relative  rounded-lg border-2 border-gray-200 ">
-          <div id="widget-header" className="max-w-full h-12 bg-white box-border align-middle p-3 relative">
+          <div id="widget-header" className="max-w-full h-11 bg-white box-border align-middle p-3 relative">
             <div id="widget-icon" className="bg-cart-icon w-6 h-6 bg-cover bg-no-repeat float-left"/>
             <div id="widget-name" className="text-primary-200 font-sans font-black ml-8">購入履歴</div>
             <img className="absolute w-5 h-1 top-1.5 right-3 hidden group-hover:block" src={Ellipsis}/>
           </div>
-          <div id="widget-body" className="h-widgetBody w-full bg-gray-50 py-2 space-y-2">
+          <div id="widget-body" className="h-widgetBody w-full bg-gray-50 space-y-1">
             {
               this.purchaseHistory.map((item, index)=>{
                 let stripe = (!(index % 2)) ? 'bg-gray-50' : 'bg-white'
                 return(
-                  <div id="widget-content-item" className={stripe + ' w-full h-20 box-border align-middle p-2.5 pl-12 relative text-gray-500'} key={index}>
-                    <div id="item-content" className="font-sans font-bold text-xs w-widgetBody">
-                      {item.category} <br/>
-                      {item.date}<br/>
-                                      請求先 : {item.billTo}<br/>
-                      <p className="text-widget-xs">支払日: {item.paymentDate}</p>
-                    </div>
+                  <div id="widget-content-item" className={stripe + ' font-sans font-bold text-xs w-full box-border align-middle h-1/3 relative text-gray-500 grid grid-cols-2'} key={index}>
+                    <div className="ml-20">購入履歴</div>
+                    <div>&nbsp;</div>
+                    <div className="ml-20">商品名</div>
+                    <div>{item.productName}</div>
+                    <div className="ml-20">購入日</div>
+                    <div>{item.purchaseDate}</div>
+                    <div className="ml-20">購入数</div>
+                    <div>{item.numberOfUnits}</div>
+                    <div className="ml-20">合計金額</div>
+                    <div>{item.amount}</div>
+                    <div className="ml-20">状態</div>
+                    <div className="" >{item.status}</div>
                   </div>
                 )
               })
