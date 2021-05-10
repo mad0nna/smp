91d97895c6d1e60dd58abb1748e80b0d91368fd7
roda <chiong.r@sprobe.com>
2021-05-10 16:22:29 +0800

    initial commit for Dashboard Settings revision
    Summary: buttons responsivesness
    
    Test Plan:
    1. patch diff
    2. open localhost/company/dashboard
    3. Check dashboard settings widget
    
    Reviewers: santos.ma
    
    Reviewed By: santos.ma
    
    Subscribers: gordo.jt
    
    Maniphest Tasks: T34601
    
    Differential Revision: https://phabricator.sprobe.ph/D25367

--- src/backend/resources/js/components/dashboardSettings.js
+++ src/backend/resources/js/components/dashboardSettings.js
@@ -12,36 +12,39 @@ const DashboardSettings =(props) => {
   if (typeof props.interActivePages != 'undefined') {
     showMoveButton = (props.interActivePages.includes(location.pathname)) ? 'group-hover:block' : ''
   }
+  const buttons = [
+    { label: '支払い方法', onClick: '', font: '3xl:text-4xl 2xl:text-3xl xl:text-2xl lg:text-lg md: text-md sm:text-md', url: '#', photo: imgMethPayment},
+    { label: 'サービスをキャンセル', onClick: '', font: '3xl:text-2xl 2xl:text-xl xl:text-lg lg:text-md md: text-sm sm:text-xs', url: '', photo: imgCancelService},
+    { label: '通知', onClick: '', font: '3xl:text-5xl 2xl:text-4xl xl:text-3xl lg:text-2xl md: text-xl sm:text-lg', url: '#', photo: imgNotice },
+    { label: 'サポート', onClick: props.showWidgets, font: '3xl:text-4xl 2xl:text-3xl xl:text-2xl lg:text-xl md: text-lg sm:text-md', url: '#', photo: imgSupport},
+  ]
+
   return(
     <div className="w-full h-full relative group">
       <div className={'absolute w-12 h-5 -top-4 px-1 pt-0.5 right-6 text-center text-gray-500 bg-white font-sans text-xxs leading-2 rounded-md border-gray-200 border-2 cursor-move hidden ' + showMoveButton}>Move</div>
-      <div className="relative flex flex-col items-center justify-center gap-4 rounded-lg border-2 border-gray-200 w-full h-full bg-white group pt-3">
-        <div className="component-header w-full h-4 pl-4">
+      <div className="relative flex flex-col items-center gap-3 rounded-lg border-2 border-gray-200 w-full h-full bg-white group pt-3">
+        <div className="w-full h-12 pl-4">
           <img className="inline" src={imgSettingsIcon}/>
           <span className="ml-2 p-0 inline text-green-600 font-bold ">設定</span>
           <img className= "float-right mr-4 hidden group-hover:block" src={Ellipsis} />
         </div>
-        <div className="justify-around w-full h-full overflow-hidden relative">
-          <div className="flex flex-wrap px-8 py-6 pb-4 h-full">
-            <div className="flex justify-center md:w-1/2 md:h-1/2 2xlg:px-1 sm:pr-2">
-              <img className= "p-0 rounded-l" src={imgMethPayment} style={{height : '80%'}} />
-            </div>
-            <div className="flex justify-center md:w-1/2 md:h-1/2 2xlg:px-1 sm:pl-2">
-              <img className= "p-0 rounded-l" src={imgCancelService} style={{height : '80%'}} />
-            </div>
-            <div className="flex justify-center md:w-1/2 md:h-1/2 2xlg:px-1 sm:pr-2">
-              <img className= "p-0 rounded-l" src={imgNotice} style={{height : '80%'}} />
-            </div>
-            <div className="cursor-pointer flex justify-center md:w-1/2 md:h-1/2 2xlg:px-1 sm:pl-2">
-              <img className= "p-0 rounded-l" src={imgSupport} style={{height : '80%'}} />
-            </div>
+        <div className=" w-full px-4 overflow-y-auto">
+          <div className="grid grid-cols-2 gap-4">
+            {
+              buttons.map((button, index) => {
+                return (
+                  <div key={index} onClick={button.onClick} className="cursor-pointer col-span-2 xl:col-span-1 h-36 bg-center bg-no-repeat bg-cover flex items-center justify-center rounded-lg" style={{ backgroundImage: `url("${button.photo}")` }}>
+                    <h3 className={'font-bold text-white ' + button.font }>{button.label}</h3>
+                  </div>
+                )
+              })
+            }
           </div>
         </div>
       </div>
     </div>
   )
 }
-
 
 export default DashboardSettings
 if (document.getElementById('dashboardSettings')){
