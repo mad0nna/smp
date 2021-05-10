58631869d0979971db45529b199de9abf4cd5bcc
roda <chiong.r@sprobe.com>
2021-05-10 11:25:45 +0800

    adjust sales graph1&2
    Summary: Add a 2nd chart once the 2nd item in the dropdown list is selected
    
    Test Plan:
    1. Patch diff
    2. Open https://localhost/sales/dashboard
    3. Click on the dropdown list at the middle top widget of the dashboard
    
    Expectation: It should show the other chart
    
    Reviewers: gordo.jt
    
    Reviewed By: gordo.jt
    
    Subscribers: gordo.jt
    
    Maniphest Tasks: T34690
    
    Differential Revision: https://phabricator.sprobe.ph/D25427

--- src/backend/resources/js/components/sales/graph2Widget.js
+++ src/backend/resources/js/components/sales/graph2Widget.js
@@ -1,52 +1,38 @@
-import React from 'react'
-import graph2 from '../../../svg/sales-graph2.svg'
-import ArrowDownSm from '../../../img/arrowdownsmall.png'
+import React, {useState} from 'react'
+import ServiceSalesGraph from '../../../svg/service-sales-graph.svg'
+import ClientCountGraph from '../../../svg/client-count-graph.svg'
 import graph2Icon from '../../../img/graph2.png'
 
 const Graph2 =() =>{
+  const [chartType,setChartType] = useState('7.1')
+  const changeChartHandler = (event) => {
+    setChartType(event.target.value)
+  }
 
   return(
-    <div className="w-full h-full relative group ">
+    <div className="w-full h-full relative group font-meiryo text-gray-700">
       <div className="w-full h-full overflow-hidden relative bg-white rounded-lg border-2 border-gray-200 ">
         <div id="widget-header" className="max-w-full h-6 bg-white box-border align-middle p-3 relative">
           <img src={graph2Icon} className="w-6 h-6 ml-4 bg-cover bg-no-repeat float-left"/>
-          <div id="widget-name" className="text-primary-200 font-sans font-bold ml-4 float-left">各サービスの売上高</div>
-          <div className="float-right mr-4">
-            <div className="table-cell h-4 w-full align-middle relative group">
-              <div id="search-bar" className="border-2 border-primary-200 h-5 rounded-xs w-44 mx-0 my-auto bg-white table-cell relative cursor-pointer">
-                <p className="mx-0 my-auto w-48 inline ml-8 text-gray-500 text-sm font-bold">顧客企業数</p>
-                <img src={ArrowDownSm} className="absolute top-3 right-3"/>
-              </div>
-              <div className="w-full h-24 bg-gray-200 absolute -bottom-18 z-10 py-1 px-8 hidden group-hover:block">
-                <div className="w-full h-12">
-                  <a href="#" className=" space-x-2 ">
-                    <div className="inline-block">
-                      <div className="table-cell align-middle h-12">
-                        <div className="inline-block text-xs my-auto mx-0 text-gray-800 text-center">顧客企業数</div>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-                <div className="w-full h-12">
-                  <a href="#" className="space-x-2">
-                    <div className="inline-block">
-                      <div className="table-cell align-middle h-12">
-                        <div className="inline-block text-xs text-gray-800 my-auto mx-0  text-center">各サービスの売上高</div>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              </div>
+          <div id="widget-name" className="text-primary-200 text-md font-meiryo font-bold ml-4 float-left">各サービスの売上高</div>
+          <div className="float-right">  
+            <div className="w-full h-8 z-10 py-1 px-8 text-xs">
+              <select className="w-full h-8 bg-white border-2 border-primary-200 px-4 lg:px-2 font-bold text-center" onChange={(e)=>changeChartHandler(e)}>
+                <option className="h-12 bg-gray-300 border-2 border-primary-200 px-auto" value="7.1">各サービスの売上高</option>
+                <option className="h-12 bg-gray-300 px-auto border-2 border-primary-200" value="7.2">顧客企業数</option>
+              </select>
             </div>
           </div>
         </div>
-        <div className="pt-0 w-full"><img className="mx-auto h-full px-4 pb-5" src={graph2}/></div>
+        <div className="pt-0 w-full">
+          {
+            <img className="mx-auto h-full px-4 pb-5" src={chartType === '7.1' ? ServiceSalesGraph : ClientCountGraph}/>
+          }
+        </div>
         
       </div>
     </div>
-
   )
-
 }
 
 export default Graph2
