d0728bc5c1c20be988a7f4ae3f1645a7d43a262f
Ivan Alcuino <alcuino.im@sprobe.com>
2021-07-29 15:43:22 +0800

    Change to functional js
    Summary: Change to functional js
    
    Test Plan:
    1. Add this code to web.php
      Route::view('admin/admin-login', 'admin.adminLogin');
    2. Go to https://localhost/admin/admin-login and it should look like this
    {F980904}
    
    Reviewers: lindo.jmj
    
    Reviewed By: lindo.jmj
    
    Maniphest Tasks: T36399
    
    Differential Revision: https://phabricator.sprobe.ph/D26720

--- src/backend/resources/js/components/admin/adminLogin.js
+++ src/backend/resources/js/components/admin/adminLogin.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import logo from '../../../img/kot-admin-panel.png'
 import lockIcon from '../../../svg/lock-icon.svg'
@@ -17,29 +17,24 @@ let validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+
 //   return valid
 // }
 
-class AdminLogin extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      email: null,
-      password: null,
-      errors: {
-        email: '',
-        password: ''
-      },
-      formError: ''
-    }
+const AdminLogin = () => {
+  // eslint-disable-next-line
+  const [state, setState] = useState({
+    email: null,
+    password: null,
+    errors: {
+      email: '',
+      password: ''
+    },
+    formError: ''
+  })
 
-    console.log('init')
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleChange(event) {
+  // eslint-disable-next-line
+  const handleChange = (event) => {
     event.preventDefault()
 
     const { name, value } = event.target
-    let errors = this.state.errors
+    let errors = state.errors
     switch (name) {
       case 'email':
         errors.email = validEmailRegex.test(value)
@@ -56,144 +51,146 @@ class AdminLogin extends Component {
         break
     }
 
-    this.setState({ errors, [name]: value, formError: '' }, () => {
-      console.log(errors)
+    setState((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+        formError: ''
+      }
     })
   }
 
-  errorClass(error) {
+  // eslint-disable-next-line
+  const errorClass = (error) => {
     return error.length === 0 ? '' : 'field-error'
   }
-
-  handleSubmit(event) {
+  // eslint-disable-next-line
+  const handleSubmit = (event) => {
     event.preventDefault()
     window.location.href = '/admin/dashboard'
     return
-    // if(validateForm(this.state.errors) && this.state.name !== null && this.state.password !== null) {
+    // if(validateForm(state.errors) && state.name !== null && state.password !== null) {
     //   console.info('Valid Form')
     //   window.location.href = '/dashboard'
     // }else{
     //   console.error('Invalid Form')
-    //   this.setState({formError: '無効な入力です'})
+    //   setState({formError: '無効な入力です'})
 
     // }
   }
 
-  render() {
-    // let errors = this.state.errors
-    return (
-      <div>
-        <img
-          src={BackgroundImage}
-          className="absolute 2xl:w-9/12 xl:w-full"
-          style={{
-            position: 'absolute',
-            left: '0px',
-            bottom: '0px',
-            overflow: 'visible'
-          }}
-        />
-        <div className="relative">
-          <div className="z-10" style={{ width: '445px' }}>
-            <div className="rounded-xl h-82 bg-white shadow-sm mt-32">
-              <div className="flex flex-wrap justify-around gap-4">
-                <img
-                  className="p-0  mt-14"
-                  src={logo}
-                  style={{ height: '42px' }}
-                />
-              </div>
+  return (
+    <div>
+      <img
+        src={BackgroundImage}
+        className="absolute 2xl:w-9/12 xl:w-full"
+        style={{
+          position: 'absolute',
+          left: '0px',
+          bottom: '0px',
+          overflow: 'visible'
+        }}
+      />
+      <div className="relative">
+        <div className="z-10" style={{ width: '445px' }}>
+          <div className="rounded-xl h-82 bg-white shadow-sm mt-32">
+            <div className="flex flex-wrap justify-around gap-4">
+              <img
+                className="p-0  mt-14"
+                src={logo}
+                style={{ height: '42px' }}
+              />
+            </div>
+            <div
+              className="text-center font-bold my-6"
+              style={{ color: '#5B5B5B' }}
+            >
+              韋駄天にログイン
+            </div>
+            <div
+              className="bg-gray-100 rounded-b-xl"
+              style={{
+                filter: 'drop-shadow(0px 3px 6px rgba(0, 0, 0, 0.161))',
+                minHeight: '314px',
+                overflow: 'auto'
+              }}
+            >
               <div
-                className="text-center font-bold my-6"
+                className="text-lg text-2xl text-center font-bold my-6 "
                 style={{ color: '#5B5B5B' }}
               >
-                韋駄天にログイン
+                <img className="inline" src={SettingIcon} />
+                <span>管理者</span>
               </div>
-              <div
-                className="bg-gray-100 rounded-b-xl"
-                style={{
-                  filter: 'drop-shadow(0px 3px 6px rgba(0, 0, 0, 0.161))',
-                  minHeight: '314px',
-                  overflow: 'auto'
-                }}
-              >
-                <div
-                  className="text-lg text-2xl text-center font-bold my-6 "
-                  style={{ color: '#5B5B5B' }}
+              <div className="flex flex-wrap gap-0 w-full justify-center">
+                <div className="flex flex-wrap gap-0 w-3/4 relative">
+                  <img
+                    className="absolute z-10"
+                    src={mailIcon}
+                    style={{ height: '12px', top: '14px', left: '15px' }}
+                  />
+                  <input
+                    type="text"
+                    name="email"
+                    placeholder="メールアドレス"
+                    style={{ padding: '8px 8px 8px 38px' }}
+                    className={
+                      'w-full px-3 py-3 placeholder-blueGray-300 relative bg-white rounded text-sm border border-gray-300 outline-none pl-9'
+                    }
+                  />
+                </div>
+                <div className="flex flex-wrap gap-0 w-3/4 relative my-4">
+                  <img
+                    className="absolute z-10"
+                    src={lockIcon}
+                    style={{ height: '12px', top: '14px', left: '19px' }}
+                  />
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="パスワード"
+                    style={{ padding: '8px 8px 8px 38px' }}
+                    className={
+                      'w-full px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative  bg-white rounded text-sm border border-gray-300 outline-none pl-9'
+                    }
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap gap-0 w-full justify-center mt-2 mb-3">
+                <button
+                  onClick={handleSubmit}
+                  className="bg-yellow-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg w-1/3"
                 >
-                  <img className="inline" src={SettingIcon} />
-                  <span>管理者</span>
-                </div>
-                <div className="flex flex-wrap gap-0 w-full justify-center">
-                  <div className="flex flex-wrap gap-0 w-3/4 relative">
-                    <img
-                      className="absolute z-10"
-                      src={mailIcon}
-                      style={{ height: '12px', top: '14px', left: '15px' }}
-                    />
-                    <input
-                      type="text"
-                      name="email"
-                      placeholder="メールアドレス"
-                      style={{ padding: '8px 8px 8px 38px' }}
-                      className={
-                        'w-full px-3 py-3 placeholder-blueGray-300 relative bg-white rounded text-sm border border-gray-300 outline-none pl-9'
-                      }
-                    />
-                  </div>
-                  <div className="flex flex-wrap gap-0 w-3/4 relative my-4">
-                    <img
-                      className="absolute z-10"
-                      src={lockIcon}
-                      style={{ height: '12px', top: '14px', left: '19px' }}
-                    />
-                    <input
-                      type="password"
-                      name="password"
-                      placeholder="パスワード"
-                      style={{ padding: '8px 8px 8px 38px' }}
-                      className={
-                        'w-full px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative  bg-white rounded text-sm border border-gray-300 outline-none pl-9'
-                      }
-                    />
-                  </div>
-                </div>
-                <div className="flex flex-wrap gap-0 w-full justify-center mt-2 mb-3">
-                  <button
-                    onClick={this.handleSubmit}
-                    className="bg-yellow-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg w-1/3"
-                  >
-                    サインイン
-                  </button>
-                </div>
+                  サインイン
+                </button>
+              </div>
 
-                {this.state.errors.email.length > 0 && (
-                  <div className="flex flex-wrap items-center justify-center  text-sm login-error ">
-                    {this.state.errors.email}
-                    <br />
-                  </div>
-                )}
-                {this.state.errors.password.length > 0 && (
-                  <div className="flex flex-wrap items-center justify-center  text-sm login-error login-error ">
-                    {this.state.errors.password}
-                  </div>
-                )}
-                {this.state.formError.length > 0 && (
-                  <div className="flex flex-wrap items-center justify-center  text-sm login-error login-error ">
-                    {this.state.formError}
-                  </div>
-                )}
-
-                <div className="text-xs text-center static bottom-10 my-3 text-gray-600 font-bold">
-                  <a href="#">パスワードをお忘れですか？</a>
+              {state.errors.email.length > 0 && (
+                <div className="flex flex-wrap items-center justify-center  text-sm login-error ">
+                  {state.errors.email}
+                  <br />
                 </div>
+              )}
+              {state.errors.password.length > 0 && (
+                <div className="flex flex-wrap items-center justify-center  text-sm login-error login-error ">
+                  {state.errors.password}
+                </div>
+              )}
+              {state.formError.length > 0 && (
+                <div className="flex flex-wrap items-center justify-center  text-sm login-error login-error ">
+                  {state.formError}
+                </div>
+              )}
+
+              <div className="text-xs text-center static bottom-10 my-3 text-gray-600 font-bold">
+                <a href="#">パスワードをお忘れですか？</a>
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default AdminLogin
