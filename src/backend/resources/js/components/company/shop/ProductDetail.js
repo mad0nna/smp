0200fc95e7f85cab47d2e15148106105a46c4bda
Jesus Lara <lara.j@sprobe.com>
2021-11-29 21:19:38 +0800

    Remove unused codes

--- src/backend/resources/js/components/company/shop/ProductDetail.js
+++ src/backend/resources/js/components/company/shop/ProductDetail.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useLocation, useHistory } from 'react-router-dom'
 
-const ProductDetail = (props) => {
+const ProductDetail = () => {
   const location = useLocation()
   const history = useHistory()
 
