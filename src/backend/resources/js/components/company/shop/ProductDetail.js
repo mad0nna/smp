7e2c8cfb320b20c3e9a8d4f9bd603f5d3658e4f8
Jesus Lara <lara.j@sprobe.com>
2021-12-06 16:24:56 +0800

    Fix image error

--- src/backend/resources/js/components/company/shop/ProductDetail.js
+++ src/backend/resources/js/components/company/shop/ProductDetail.js
@@ -190,7 +190,7 @@ const ProductDetail = () => {
                   <div className="grid col-span-1 text-center flex content-center">
                     <img
                       className="w-auto h-auto p-3 tex-center m-auto"
-                      src={isLoaded ? productDetail.imgSrc : ''}
+                      src={isLoaded ? `/aimeos/${productDetail.imgSrc}` : ''}
                     ></img>
                   </div>
                   <div className="grid col-span-1 text-center flex content-center">
