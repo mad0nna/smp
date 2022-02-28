7ab572fad6e740fbdc805da78938e3c60f031681
Jesus Lara <lara.j@sprobe.com>
2022-02-28 23:46:03 +0800

    Change text to japanese "Unable to proceed cart. No stock available"

--- src/backend/resources/js/components/company/shop/ProductDetail.js
+++ src/backend/resources/js/components/company/shop/ProductDetail.js
@@ -129,7 +129,7 @@ const ProductDetail = (props) => {
   const handleCartListPage = () => {
     // create cart items
     if (productDetail.defaultStock == 0) {
-      alert('Unable to proceed cart. No stocks available.')
+      alert('カートに追加できませんでした。在庫がありません。')
       return false
     }
 
