0a15fdfd965831a58603a6f83b6ff434cdf75ea3
Ivan M. Alcuino <alcuino.im@sprobe.com>
2022-03-03 14:19:15 +0800

    Make the product list image not stretched ang removed no translate

--- src/backend/resources/views/layouts/company.blade.php
+++ src/backend/resources/views/layouts/company.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="notranslate" translate="no">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="google" content="notranslate" >
