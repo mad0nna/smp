37f65f496d3a250214d3f50d6385206f221d5ff0
Brix Rodman B. Nessia <nessia.brb@ad.sprobe.ph>
2022-03-23 20:02:42 +0800

    Bug Fix

--- src/backend/resources/js/components/company/shop/ProductList.js
+++ src/backend/resources/js/components/company/shop/ProductList.js
@@ -297,8 +297,8 @@ const ProductList = () => {
         }
 
         prodDescription =
-          prodDescription.length >= 65
-            ? `${prodDescription.substring(0, 62)}...`
+          prodDescription.length >= 80
+            ? `${prodDescription.substring(0, 80)}...`
             : prodDescription
 
         return state.loaded ? (
