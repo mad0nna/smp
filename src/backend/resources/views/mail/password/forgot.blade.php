a827133f890be1a91aadb93457092998d02323e8
Mark Anthony Santos <santos.ma@sprobe.com>
2022-05-05 13:27:02 +0800

    client feedback

--- src/backend/resources/views/mail/password/forgot.blade.php
+++ src/backend/resources/views/mail/password/forgot.blade.php
@@ -1,16 +1,17 @@
 @component('mail::message')
-平素よりお世話になっております。​<br/>
-KING OF TIMEサポートセンターです。<br/>
+{{ $user->last_name . ' ' . $user->first_name}}様<br/>
 <br/>
+平素よりお世話になっております。​<br/>
+KING OF TIMEサポートセンターです。​<br/>​<br/>
 
-パスワード再設定のご要望を承りました。<br/>
+パスワード再設定のご要望を承りました。​<br/><br/>
 
-下記ボタンをクリックして、パスワードの再設定を行ってください。​<br/>
+下記ボタンをクリックして、パスワードの再設定を行ってください。​<br/><br/>
 
 @component('mail::button', ['url' => $url])
 パスワード再設定​
 @endcomponent
 
 ※本メールは自動配信されております。<br/>
-お心当たりがない場合は、ご容赦ください。
+&nbsp;&nbsp;お心当たりがない場合は、ご容赦ください。
 @endcomponent
