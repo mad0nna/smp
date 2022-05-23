23b53b69d91c5b2dbd65bc0302aa70af8c02851a
Ivan M. Alcuino <alcuino.im@sprobe.com>
2022-05-23 17:00:45 +0800

    applied request changes

--- src/backend/resources/js/components/Welcome.js
+++ src/backend/resources/js/components/Welcome.js
@@ -1,26 +1,40 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Welcome = () => {
   const [state, setState] = useState({
+    loading: true,
     lastName: ''
   })
-  let userData = JSON.parse(document.getElementById('userData').textContent)
   useEffect(() => {
-    setState({
-      loading: false,
-      lastName: userData.lastName
+    axios.get(location.origin + '/getLoggedinUser').then((response) => {
+      if (response.status === 200) {
+        setState({
+          loading: false,
+          lastName: response['data']['contactLastName']
+        })
+      }
     })
-  }, [state.lastName])
+  }, [])
   return (
     <div className="w-full">
-      <h1 className="text-green-600 text-lg font-bold tracking-normal">
-        ようこそ{' '}
-        <span className="capitalize text-2xl text-green-700">
-          {state.lastName}
-        </span>
-        {' 様'}
-      </h1>
+      {!state.loading ? (
+        <h1 className="text-green-600 text-lg font-bold tracking-normal">
+          ようこそ{' '}
+          <span className="capitalize text-2xl text-green-700">
+            {state.lastName}
+          </span>
+          {' 様'}
+        </h1>
+      ) : (
+        ''
+      )}
     </div>
+    // <div className="w-full">
+    //   <h1 className="text-green-700 text-3xl tracking-normal">
+    //     ダッシュボード
+    //   </h1>
+    // </div>
   )
 }
 export default Welcome
