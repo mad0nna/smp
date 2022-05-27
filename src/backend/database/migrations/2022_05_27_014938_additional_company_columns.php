714487c0fad060594fd852bbf8889aea66ac561c
Mark Anthony Santos <santos.ma@sprobe.com>
2022-05-27 09:51:08 +0800

    transfer new columns in new migration file

---
+++ src/backend/database/migrations/2022_05_27_014938_additional_company_columns.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AdditionalCompanyColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('company', function($table)
+        {
+            $table->string('server_name')->nullable();
+            $table->string('phase')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
