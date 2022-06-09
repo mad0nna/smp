09af28c2ceb0c9332276d5022f04182edc88429c
John Lorenz Ruizo <ruizo.jl@sprobe.com>
2022-06-09 17:26:27 +0800

    Added wildcard csp img-src directive.

--- src/backend/resources/views/vendor/shop/jqadm/index.blade.php
+++ src/backend/resources/views/vendor/shop/jqadm/index.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta http-equiv="Content-Security-Policy" content="default-src 'self' data: blob: https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://dxpf-invoice.s3.ap-northeast-1.amazonaws.com; style-src 'unsafe-inline' 'self' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net; script-src 'unsafe-eval' 'self' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net; img-src 'self' data: blob: https://smp-staging.s3.ap-northeast-1.amazonaws.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://*.tile.openstreetmap.org https://aimeos.org; frame-src https://www.youtube.com">
+		<meta http-equiv="Content-Security-Policy" content="default-src 'self' data: blob: https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://dxpf-invoice.s3.ap-northeast-1.amazonaws.com; style-src 'unsafe-inline' 'self' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net; script-src 'unsafe-eval' 'self' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net; img-src * 'self' data: blob: https://smp-staging.s3.ap-northeast-1.amazonaws.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://*.tile.openstreetmap.org https://aimeos.org; frame-src https://www.youtube.com">
 
 		<title>{{ config('app.name', 'Idaten') }}</title>
 		<link rel="shortcut icon" href="{{ asset('images/kotFabIcon.png') }}">
