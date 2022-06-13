9a6c5c7d81360da9b55f29d834ce8a479e47476a
Mark Anthony Santos <santos.ma@sprobe.com>
2022-06-13 12:36:25 +0800

    hide other dropdown item in admin nav

--- src/backend/resources/js/components/Navigation.js
+++ src/backend/resources/js/components/Navigation.js
@@ -284,14 +284,14 @@ const Navigation = () => {
           //   iconSize: 'h-5 w-5',
           //   extraStyle: 'cursor-default'
           // },
-          {
-            label: '設定',
-            url: '/admin/settings',
-            iconNormal: 'bg-settings-icon-white',
-            iconHover: '',
-            iconSize: 'h-5 w-5',
-            extraStyle: ''
-          },
+          // {
+          //   label: '設定',
+          //   url: '/admin/settings',
+          //   iconNormal: 'bg-settings-icon-white',
+          //   iconHover: '',
+          //   iconSize: 'h-5 w-5',
+          //   extraStyle: ''
+          // },
           {
             label: 'ログアウト',
             url: '/logout',
