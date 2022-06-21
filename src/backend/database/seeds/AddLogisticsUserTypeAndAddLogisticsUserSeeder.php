a5dcc9fad6d07851cb08e39d405a945ccc4782ba
lindo.jmj <lindo.jmj@sprobe.com>
2022-06-21 12:45:20 +0800

    Optimized seeder

--- src/backend/database/seeds/AddLogisticsUserTypeAndAddLogisticsUserSeeder.php
+++ src/backend/database/seeds/AddLogisticsUserTypeAndAddLogisticsUserSeeder.php
@@ -21,23 +21,36 @@ class AddLogisticsUserTypeAndAddLogisticsAdminSeeder extends Seeder
             'type_alias' => ''
         ];
 
-        $status = UserStatus::where('name', config('user.statuses.active'))->first();
-        $logisticUserType = UserType::create($data);
+        $userTypeExists = UserType::where('name', 'Logistics Admin')->first();
 
-        User::create([
-            // 'account_code' => '-',
-            'username' => 'logistics@sprobe.com',
-            'email' => 'logistics@sprobe.com',
-            'contact_num' => '12345678',
-            'password' => Hash::make('Password2021!'),
-            // 'company_id' => '1',  // maybe can be removed?
-            'first_name' => 'Logistics',
-            'last_name' => 'Admin',
-            'user_status_id' => $status->id,
-            'user_type_id' => $logisticUserType->id,
-            'email_verified_at' => Carbon::now(),
-            'name' => 'Logistics Admin',
-            ]
-        );
+        if (!$userTypeExists instanceof UserType) {
+            $logisticUserType = UserType::create($data);
+        } else {
+            $this->command->comment('User type is already existing, seeding attempt was ignored.');
+        }
+
+        $userExists = User::where('username', 'logistics@sprobe.com')->first();
+
+        if (!$userExists instanceof User) {
+            $status = UserStatus::where('name', config('user.statuses.active'))->first();
+
+            User::create([
+                // 'account_code' => '-',
+                'username' => 'logistics@sprobe.com',
+                'email' => 'logistics@sprobe.com',
+                'contact_num' => '12345678',
+                'password' => Hash::make('Password2021!'),
+                // 'company_id' => '1',  // maybe can be removed?
+                'first_name' => 'Logistics',
+                'last_name' => 'Admin',
+                'user_status_id' => $status->id,
+                'user_type_id' => $logisticUserType->id,
+                'email_verified_at' => Carbon::now(),
+                'name' => 'Logistics Admin',
+                ]
+            );
+        } else {
+            $this->command->comment('User is already existing, seeding attempt was ignored.');
+        }
     }
 }
