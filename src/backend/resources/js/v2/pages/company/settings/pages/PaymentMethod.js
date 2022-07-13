8216e8997e7715a5667c21bda6ce275b02c62d03
chu.rbr <chu.rbr@sprobe.com>
2022-07-13 14:54:11 +0800

    Show and hide card details form on payment method selection

--- src/backend/resources/js/v2/pages/company/settings/pages/PaymentMethod.js
+++ src/backend/resources/js/v2/pages/company/settings/pages/PaymentMethod.js
@@ -13,7 +13,7 @@ const PaymentMethod = () => {
   return (
     <>
       <h3 className="mt-22px ml-22px font-medium text-23px text-primary-600">
-        お支払い方法
+        お支払い方法 {method}
       </h3>
       <div>
         <div className="flex flex-col h-fit justify-center lg:min-h-500px lg:mt-95px">
@@ -80,71 +80,75 @@ const PaymentMethod = () => {
           </div>
           {/* endregion */}
           {/* region > Payment form */}
-          <div className="flex justify-center">
-            <div className="w-full bg-hex-F1F1F1 rounded-xl mt-62px mb-25px mx-3 lg:w-590px lg:mx-0">
-              <h3 className="font-medium text-22px pl-20px py-11px border-b border-hex-D8D8D8">
-                クレジットカード
-              </h3>
-              <div className="flex flex-col lg:table mt-42px">
-                <p className="table-row">
-                  <label
-                    htmlFor="card_type"
-                    className="pr-13px table-cell text-hex-3A3A3A lg:pl-55px"
-                  >
-                    クレジットカード種別
-                  </label>
-                  <input
-                    type="text"
-                    id="card_type"
-                    className="w-4/5 lg:w-318px lg:mr-0 h-48px outline-none table-cell rounded-xs mb-4 bg-hex-E8E8E8"
-                  />
-                </p>
-                <p className="table-row">
-                  <label
-                    htmlFor="card_number"
-                    className="table-cell text-hex-3A3A3A lg:pl-55px"
-                  >
+          {method === 'card' && (
+            <>
+              <div className="flex justify-center">
+                <div className="w-full bg-hex-F1F1F1 rounded-xl mt-62px mb-25px mx-3 lg:w-590px lg:mx-0">
+                  <h3 className="font-medium text-22px pl-20px py-11px border-b border-hex-D8D8D8">
                     クレジットカード
-                  </label>
-                  <input
-                    type="text"
-                    id="card_number"
-                    className="w-4/5 lg:w-318px lg:mr-0 h-48px outline-none table-cell rounded-xs mb-4 bg-hex-E8E8E8"
-                  />
-                </p>
-                <p className="table-row">
-                  <label
-                    htmlFor="card_expiration"
-                    className="table-cell text-hex-3A3A3A lg:pl-55px"
-                  >
-                    有効期限日
-                  </label>
-                  <input
-                    type="text"
-                    id="card_expiration"
-                    className="w-143px h-48px outline-none table-cell rounded-xs mb-4 bg-hex-E8E8E8"
-                  />
-                  <p className="border border-hex-FF9898 mb-9px w-143px h-28px text-11px text-hex-FF0000 bg-hex-FEE5E5 leading-7 rounded-3px">
-                    <ExclamationIcon
-                      className="w-4 h-4 ml-7px mr-2.5 inline"
-                      fill="#FF0000"
-                    />
-                    Card has expired!
-                  </p>
-                </p>
+                  </h3>
+                  <div className="flex flex-col lg:table mt-42px">
+                    <p className="table-row">
+                      <label
+                        htmlFor="card_type"
+                        className="pr-13px table-cell text-hex-3A3A3A lg:pl-55px"
+                      >
+                        クレジットカード種別
+                      </label>
+                      <input
+                        type="text"
+                        id="card_type"
+                        className="w-4/5 lg:w-318px lg:mr-0 h-48px outline-none table-cell rounded-xs mb-4 bg-hex-E8E8E8"
+                      />
+                    </p>
+                    <p className="table-row">
+                      <label
+                        htmlFor="card_number"
+                        className="table-cell text-hex-3A3A3A lg:pl-55px"
+                      >
+                        クレジットカード
+                      </label>
+                      <input
+                        type="text"
+                        id="card_number"
+                        className="w-4/5 lg:w-318px lg:mr-0 h-48px outline-none table-cell rounded-xs mb-4 bg-hex-E8E8E8"
+                      />
+                    </p>
+                    <p className="table-row">
+                      <label
+                        htmlFor="card_expiration"
+                        className="table-cell text-hex-3A3A3A lg:pl-55px"
+                      >
+                        有効期限日
+                      </label>
+                      <input
+                        type="text"
+                        id="card_expiration"
+                        className="w-143px h-48px outline-none table-cell rounded-xs mb-4 bg-hex-E8E8E8"
+                      />
+                      <p className="border border-hex-FF9898 mb-9px w-143px h-28px text-11px text-hex-FF0000 bg-hex-FEE5E5 leading-7 rounded-3px">
+                        <ExclamationIcon
+                          className="w-4 h-4 ml-7px mr-2.5 inline"
+                          fill="#FF0000"
+                        />
+                        Card has expired!
+                      </p>
+                    </p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="flex justify-center mb-72px">
-            <div className="w-590px text-right">
-              <button
-                onClick={handleModalClose}
-                className="bg-hex-0ABBB5 w-202px h-51px text-white text-20px rounded-8px mr-3 lg:mr-0"
-              >
-                保存する
-              </button>
-            </div>
-          </div>
+              <div className="flex justify-center mb-72px">
+                <div className="w-590px text-right">
+                  <button
+                    onClick={handleModalClose}
+                    className="bg-hex-0ABBB5 w-202px h-51px text-white text-20px rounded-8px mr-3 lg:mr-0"
+                  >
+                    保存する
+                  </button>
+                </div>
+              </div>
+            </>
+          )}
           {/* endregion */}
         </div>
         {modalOn ? (
