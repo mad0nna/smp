63e8ba0c2cefd36c3ef0bc03dd05d383e66ab759
Jon Miguel J. Lindo <lindo.jmj@sprobe.com>
2022-07-18 05:52:00 +0000

    SMP-47: UI/UX revamp fixes

--- src/backend/resources/js/components/company/shop/CartList.js
+++ src/backend/resources/js/components/company/shop/CartList.js
@@ -950,9 +950,9 @@ const CartList = () => {
       .then((res) => setCsrfToken(res.data.meta.csrf))
   }, [])
   return (
-    <div className="bg-primaryBg grid lg:grid-cols-4 md:grid-cols-2 gap-6 mx-10 mt-5 font-meiryo">
-      <div className="md:col-span-1 lg:col-span-3 pb-5">
-        <div className="w-full rounded-lg shadow-xl overflow-hidden bg-white mb-10">
+    <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-6 font-meiryo">
+      <div className="md:col-span-1 lg:col-span-3">
+        <div className="w-full overflow-hidden">
           <div className="px-3 pt-3 pb-6">
             <div className="pb-2 border-b border-green-800 border-opacity-80 flex space-x-4 items-center">
               <div className="bg-cart-icon h-10 w-8"></div>
@@ -987,7 +987,7 @@ const CartList = () => {
         </div>
       </div>
       <div className="w-full h-full relative group overflow-hidden">
-        <div className="relative gap-3 bg-white rounded-2xl border-2 border-gray-400 shadow-xl w-full">
+        <div className="relative gap-3 bg-white rounded-xl border-2 border-gray-400 w-full">
           <div className="w-full px-1">
             <div className="grid gap-6 grid-cols-1 p-7 items-center justify-center">
               <div className="flex flex-wrap space-x-4 justify-between pt-10 pb-5">
