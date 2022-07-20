ee396b25542491cbd6b8e28ff56f2d8c3598bb6c
Brix Rodman B. Nessia <nessia.brb@AD.SPROBE.PH>
2022-07-20 16:25:50 +0800

    Fix bg issue on company pages

--- src/backend/resources/views/accountslist.blade.php
+++ src/backend/resources/views/accountslist.blade.php
@@ -1,5 +1,12 @@
-@extends('layouts.v2.company')
+@extends('layouts.v2.company', ['useCustomContainer' => true])
 
 @section('content')
-<div id="company-accounts-list"></div>
+<div class="col-span-full bg-hex-F7F8F9">
+    <div class="mb-5 mx-11 mt-10 rounded-xl bg-hex-F7F8F9 h-fit">
+        <div id="company-accounts-list"></div>
+    </div>
+</div>
 @endsection
+
+
+
