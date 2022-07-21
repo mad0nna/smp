dca4339e7b771803964efb67decfa3fc21521d38
Brix Rodman B. Nessia <nessia.brb@AD.SPROBE.PH>
2022-07-21 19:13:23 +0800

    Fix Border Issue

--- src/backend/resources/views/companyBilling.blade.php
+++ src/backend/resources/views/companyBilling.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="col-span-full bg-hex-F7F8F9">
-    <div class="mb-5 mx-11 mt-10 rounded-xl bg-hex-F7F8F9 h-fit">
+    <div class="mb-5 mx-11 mt-10 rounded-xl bg-white h-fit border border-whiteTint-500 rounded-xl">
         <div id="companyBilling"></div>
     </div>
 </div>
